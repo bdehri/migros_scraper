cb63fd62ea868baff056354384d9274072948d6e
bdehri <bdehrioglu@yahoo.com>
2020-08-16 13:18:53 +0300

    feat: adds application logic - adds requests to endpoints - adds html parsing - adds csv output

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import json
+
+import pandas as pd
+import numpy as np
 
 base_url="https://www.migros.com.tr"
 
-category_suffix=["gazli-icecek-c-80"]
-
+category_suffix=["gazli-icecek-c-80","gazsiz-icecek-c-81","cay-c-475","kahve-c-476","su-c-84","maden-suyu-c-85"]
+array_of_items=[]
 
 for category in category_suffix:
     i=1
     while True:
         endpoint=base_url+"/"+category+"?sayfa={}".format(i)
+        print(endpoint)
         r=requests.get(endpoint).text
         soup_output = bs(r,"html.parser")
-        page_items_json=soup_output.findAll("script")[1]
-        break
+        page_items_string_raw=soup_output.findAll("script")[1].string
+        page_items_json_raw=json.loads(page_items_string_raw)
+        items_json_array=page_items_json_raw["mainEntity"]["offers"]["itemOffered"]
+        
+        if len(items_json_array)==0:
+            break
+
+        for item in items_json_array:
+            obj={}
+            obj["İsim"]=item["name"]
+            obj["Kategori"]=item["category"]
+            obj["Fiyat"]=item["offers"]["price"]
+            obj["url"]=item["url"]
+            obj["Marka"]=item["brand"]["name"]
+            array_of_items.append(obj)
+        i+=1
+            
+data_frame=pd.DataFrame(array_of_items)
+data_frame.to_csv("items.csv")
+
+        
+        
